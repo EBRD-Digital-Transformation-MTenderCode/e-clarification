284e25902e42a3dfac32f27511b3a49a3df80f9b
Oleh Herashchenko <joghua@gmail.com>
2017-12-01 15:08:23 +0200

    refactor

--- src/main/java/com/procurement/clarification/service/EnquiryService.java
+++ src/main/java/com/procurement/clarification/service/EnquiryService.java
@@ -6,6 +6,5 @@ import org.springframework.stereotype.Service;
 
 @Service
 public interface EnquiryService {
-    //void insertData(DataDto dataDto);
     EnquiryEntity saveEnquiry(DataDto dataDto);
 }

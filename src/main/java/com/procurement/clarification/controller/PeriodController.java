0494a8bbb2a7261e486094198ed2b9bf5cefe85f
andreykolchev <andreykolchev@gmail.com>
2018-01-27 21:26:07 +0200

    code refactoring

--- src/main/java/com/procurement/clarification/controller/PeriodController.java
+++ src/main/java/com/procurement/clarification/controller/PeriodController.java
@@ -21,7 +21,7 @@ public class PeriodController {
         this.periodService = periodService;
     }
 
-    @PostMapping
+    @PostMapping("/save")
     public ResponseEntity<ResponseDto> savePeriod(@RequestParam("cpid") final String cpid,
                                                   @RequestParam("country") final String country,
                                                   @RequestParam("pmd") final String pmd,

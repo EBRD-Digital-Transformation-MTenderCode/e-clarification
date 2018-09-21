70318764a6d0f67c33b824a14ea662c3f2e3101a
andreykolchev <andreykolchev@gmail.com>
2018-09-21 18:38:56 +0300

    period

--- src/main/kotlin/com/procurement/clarification/ClarificationApplication.kt
+++ src/main/kotlin/com/procurement/clarification/ClarificationApplication.kt
@@ -3,10 +3,8 @@ package com.procurement.clarification
 import com.procurement.clarification.config.ApplicationConfig
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient
 
 @SpringBootApplication(scanBasePackageClasses = [ApplicationConfig::class])
-@EnableEurekaClient
 class ClarificationApplication
 
 fun main(args: Array<String>) {

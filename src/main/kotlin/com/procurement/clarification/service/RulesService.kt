1158a2c62a827b5b61422b129f323f2ae8c49851
andreykolchev <andreykolchev@gmail.com>
2018-09-25 09:34:00 +0300

    scale

--- src/main/kotlin/com/procurement/clarification/service/RulesService.kt
+++ src/main/kotlin/com/procurement/clarification/service/RulesService.kt
@@ -7,11 +7,11 @@ import org.springframework.stereotype.Service
 
 interface RulesService {
 
-    fun getInterval(country: String, method: String): Long
+    fun getInterval(country: String, pmd: String): Long
 
-    fun getOffset(country: String, method: String): Long
+    fun getOffset(country: String, pmd: String): Long
 
-    fun getOffsetExtended(country: String, method: String): Long
+    fun getOffsetExtended(country: String, pmd: String): Long
 
     fun getIntervalBefore(country: String, pmd: String): Long
 
@@ -20,27 +20,29 @@ interface RulesService {
 @Service
 class RulesServiceImpl(private val rulesDao: RulesDao) : RulesService {
 
-    override fun getInterval(country: String, method: String): Long {
-        return rulesDao.getValue(country, method, PARAMETER_INTERVAL)?.toLongOrNull()
+    override fun getInterval(country: String, pmd: String): Long {
+        return rulesDao.getValue(country, pmd, PARAMETER_INTERVAL)?.toLongOrNull()
                 ?: throw ErrorException(ErrorType.INTERVAL_RULES_NOT_FOUND)
     }
 
-    override fun getOffset(country: String, method: String): Long {
-        return rulesDao.getValue(country, method, PARAMETER_OFFSET)?.toLongOrNull()
+    override fun getOffset(country: String, pmd: String): Long {
+        return rulesDao.getValue(country, pmd, PARAMETER_OFFSET)?.toLongOrNull()
                 ?: throw ErrorException(ErrorType.OFFSET_RULES_NOT_FOUND)
     }
 
-    override fun getOffsetExtended(country: String, method: String): Long {
-        return rulesDao.getValue(country, method, PARAMETER_OFFSET_EXTENDED)?.toLongOrNull()
+    override fun getOffsetExtended(country: String, pmd: String): Long {
+        return rulesDao.getValue(country, pmd, PARAMETER_OFFSET_EXTENDED)?.toLongOrNull()
                 ?: throw ErrorException(ErrorType.OFFSET_RULES_NOT_FOUND)
     }
 
     override fun getIntervalBefore(country: String, pmd: String): Long {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return rulesDao.getValue(country, pmd, PARAMETER_INTERVAL_BEFORE)?.toLongOrNull()
+                ?: throw ErrorException(ErrorType.INTERVAL_RULES_NOT_FOUND)
     }
 
     companion object {
         private const val PARAMETER_INTERVAL = "interval"
+        private const val PARAMETER_INTERVAL_BEFORE = "interval_before"
         private const val PARAMETER_OFFSET = "offset"
         private const val PARAMETER_OFFSET_EXTENDED = "offsetExtended"
     }

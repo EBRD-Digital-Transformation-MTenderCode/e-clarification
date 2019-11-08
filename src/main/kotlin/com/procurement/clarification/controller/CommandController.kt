cafbdb090f370ff81afce251a2791d1e5a5a34b0
Maxim Sambulat <maxim.sambulat@gmail.com>
2019-11-08 16:18:35 +0200

    ES-2179 Added logging to controller.

--- src/main/kotlin/com/procurement/clarification/controller/CommandController.kt
+++ src/main/kotlin/com/procurement/clarification/controller/CommandController.kt
@@ -2,21 +2,46 @@ package com.procurement.clarification.controller
 
 import com.procurement.clarification.exception.EnumException
 import com.procurement.clarification.exception.ErrorException
-import com.procurement.clarification.model.dto.bpe.*
+import com.procurement.clarification.model.dto.bpe.CommandMessage
+import com.procurement.clarification.model.dto.bpe.ResponseDto
+import com.procurement.clarification.model.dto.bpe.getEnumExceptionResponseDto
+import com.procurement.clarification.model.dto.bpe.getErrorExceptionResponseDto
+import com.procurement.clarification.model.dto.bpe.getExceptionResponseDto
 import com.procurement.clarification.service.CommandService
+import com.procurement.clarification.utils.toJson
+import com.procurement.clarification.utils.toObject
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.validation.annotation.Validated
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseBody
+import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.bind.annotation.RestController
 
 @Validated
 @RestController
 @RequestMapping("/command")
 class CommandController(private val commandService: CommandService) {
+    companion object {
+        private val log: Logger = LoggerFactory.getLogger(CommandController::class.java)
+    }
 
     @PostMapping
-    fun command(@RequestBody cm: CommandMessage): ResponseEntity<ResponseDto> {
-        return ResponseEntity(commandService.execute(cm), HttpStatus.OK)
+    fun command(@RequestBody requestBody: String): ResponseEntity<ResponseDto> {
+        if (log.isDebugEnabled)
+            log.debug("RECEIVED COMMAND: '$requestBody'.")
+        val cm: CommandMessage = toObject(CommandMessage::class.java, requestBody)
+
+        val response = commandService.execute(cm)
+
+        if (log.isDebugEnabled)
+            log.debug("RESPONSE (operation-id: '${cm.context.operationId}'): '${toJson(response)}'.")
+        return ResponseEntity(response, HttpStatus.OK)
     }
 
     @ResponseBody
@@ -30,6 +55,3 @@ class CommandController(private val commandService: CommandService) {
         }
     }
 }
-
-
-

ef99c87b4451311179230770a3372e6394ee46f6
Yevhenii Skoryi <yevheniiskoryi@gmail.com>
2020-05-20 16:07:25 +0300

    ES-3307 added 'Command2Service' and 'Command2Controller'

---
+++ src/main/kotlin/com/procurement/clarification/service/Command2Service.kt
@@ -0,0 +1,46 @@
+package com.procurement.clarification.service
+
+import com.fasterxml.jackson.databind.JsonNode
+import com.procurement.clarification.application.service.Logger
+import com.procurement.clarification.infrastructure.dto.ApiResponse
+import com.procurement.clarification.infrastructure.handler.find.equiryids.FindEnquiryIdsHandler
+import com.procurement.clarification.model.dto.bpe.Command2Type
+import com.procurement.clarification.model.dto.bpe.errorResponse
+import com.procurement.clarification.model.dto.bpe.getAction
+import com.procurement.clarification.model.dto.bpe.getId
+import com.procurement.clarification.model.dto.bpe.getVersion
+import org.springframework.stereotype.Service
+
+@Service
+class Command2Service(
+    private val logger: Logger,
+    private val findEnquiryIdsHandler: FindEnquiryIdsHandler
+) {
+
+    fun execute(request: JsonNode): ApiResponse {
+
+        val version = request.getVersion()
+            .doOnError { versionError ->
+                val id = request.getId()
+                    .doOnError { _ -> return errorResponse(fail = versionError) }
+                    .get
+                return errorResponse(fail = versionError, id = id)
+            }
+            .get
+
+        val id = request.getId()
+            .doOnError { error -> return errorResponse(fail = error, version = version) }
+            .get
+
+        val action = request.getAction()
+            .doOnError { error -> return errorResponse(id = id, version = version, fail = error) }
+            .get
+
+        val response: ApiResponse = when (action) {
+            Command2Type.FIND_ENQUIRY_IDS -> findEnquiryIdsHandler.handle(node = request)
+        }
+
+        logger.info("DataOfResponse: '$response'.")
+        return response
+    }
+}

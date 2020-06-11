3a132794beccf298bd32db6cd0f826ea8be76914
Yevhenii Skoryi <yevheniiskoryi@gmail.com>
2020-06-11 12:48:11 +0300

    ES-3487 added application level dto, converter, handler for command 'findEnquiries'

---
+++ src/main/kotlin/com/procurement/clarification/application/model/dto/params/FindEnquiriesParams.kt
@@ -0,0 +1,30 @@
+package com.procurement.clarification.application.model.dto.params
+
+import com.procurement.clarification.application.model.parseCpid
+import com.procurement.clarification.application.model.parseOcid
+import com.procurement.clarification.domain.fail.error.DataErrors
+import com.procurement.clarification.domain.model.Cpid
+import com.procurement.clarification.domain.model.Ocid
+import com.procurement.clarification.domain.util.Result
+import com.procurement.clarification.domain.util.asSuccess
+
+class FindEnquiriesParams private constructor(
+    val cpid: Cpid,
+    val ocid: Ocid,
+    val isAnswer: Boolean?
+) {
+    companion object {
+        fun tryCreate(
+            cpid: String,
+            ocid: String,
+            isAnswer: Boolean?
+        ): Result<FindEnquiriesParams, DataErrors> {
+            val parsedCpid = parseCpid(value = cpid)
+                .orForwardFail { fail -> return fail }
+            val parsedOcid = parseOcid(value = ocid)
+                .orForwardFail { fail -> return fail }
+            return FindEnquiriesParams(cpid = parsedCpid, isAnswer = isAnswer, ocid = parsedOcid)
+                .asSuccess()
+        }
+    }
+}

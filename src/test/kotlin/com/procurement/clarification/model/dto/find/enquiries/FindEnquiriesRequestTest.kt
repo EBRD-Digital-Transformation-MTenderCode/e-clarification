81da4385f140186865d333b776b11dc10f23d0c1
Yevhenii Skoryi <yevheniiskoryi@gmail.com>
2020-06-11 12:04:18 +0300

    ES-3487 created request and response dto with mapping tests for command 'findEnquiries'

---
+++ src/test/kotlin/com/procurement/clarification/model/dto/find/enquiries/FindEnquiriesRequestTest.kt
@@ -0,0 +1,18 @@
+package com.procurement.clarification.model.dto.find.enquiries
+
+import com.procurement.clarification.infrastructure.handler.find.enquiries.FindEnquiriesRequest
+import com.procurement.clarification.model.dto.AbstractDTOTestBase
+import org.junit.Test
+
+class FindEnquiriesRequestTest : AbstractDTOTestBase<FindEnquiriesRequest>(FindEnquiriesRequest::class.java) {
+
+    @Test
+    fun fully() {
+        testBindingAndMapping("json/dto/find/enquirires/find_enquiries_request_full.json")
+    }
+    @Test
+    fun fully1() {
+        testBindingAndMapping("json/dto/find/enquirires/find_enquiries_request_1.json")
+    }
+
+}
